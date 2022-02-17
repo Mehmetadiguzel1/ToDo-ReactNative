bc020aa3d1ae2914f9b5c70b423a08c87e75e473
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-02-17 17:23:13 -0500

    Taks.js created for ToDo List

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Word</Text>
-      <StatusBar style="auto" />
+      
+      {/* Tasks */}
+      <View style={styles.tasksWrapper}>
+
+        <Text style={styles.sectionTitle}> Tasks </Text>
+        <View style={styles.items}>
+          {/* This is where the tasks */}
+        
+        <Task text={'Tasks 1'}/>
+        <Task text={'Tasks 2'}/>
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -13,8 +25,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    padding: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {},
 });
